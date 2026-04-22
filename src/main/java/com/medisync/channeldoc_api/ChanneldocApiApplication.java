4d0ec6366e492dd073c6b4cc5e602648ee26d310
Pubudu-Karunanayake <pubudu.karunanayake01@gmail.com>
2026-04-22 16:08:04 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/medisync/channeldoc_api/ChanneldocApiApplication.java
@@ -0,0 +1,13 @@
+package com.medisync.channeldoc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanneldocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanneldocApiApplication.class, args);
+	}
+
+}
