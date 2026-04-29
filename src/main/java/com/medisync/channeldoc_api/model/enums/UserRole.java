fc1590baa9b0f06531a8ab588eff428c1c246502
Pubudu-Karunanayake <pubudu.karunanayake01@gmail.com>
2026-04-29 16:58:58 +0530

    Refactured UserRole Enum

--- src/main/java/com/medisync/channeldoc_api/model/enums/UserRole.java
+++ src/main/java/com/medisync/channeldoc_api/model/enums/UserRole.java
@@ -3,7 +3,7 @@ package com.medisync.channeldoc_api.model.enums;
 public enum UserRole {
     ROLE_DOCTOR,
     ROLE_PATIENT,
-    ROLE_SUPERADMIN,
-    ROLE_HOSPITALADMIN,
-    ROLE_HOSPITALMANAGEMENT
+    ROLE_SUPER_ADMIN,
+    ROLE_HOSPITAL_ADMIN,
+    ROLE_HOSPITAL_MANAGEMENT
 }
