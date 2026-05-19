3c8ea45fa24f01ef492b5e2c9542a367e96ba9bb
Pubudu-Karunanayake <pubudu.karunanayake01@gmail.com>
2026-05-20 05:17:45 +0530

    refactor(dependent_profile):add dependent_profile repository and service interface

---
+++ src/main/java/com/medisync/channeldoc_api/service/DependentProfileService.java
@@ -0,0 +1,10 @@
+package com.medisync.channeldoc_api.service;
+
+import com.medisync.channeldoc_api.model.DependentProfile;
+import com.medisync.channeldoc_api.model.User;
+
+import java.util.List;
+
+public interface DependentProfileService {
+
+}
